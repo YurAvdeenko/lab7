d56d3c60dc20f47b11f5b29166ae8d8229a7bc1d
YuryAvdeenko <avdeenko.yura@mail.ru>
2021-04-14 20:39:30 +0300

    Add arrangement of message and fraim elements

--- src/bsu/rfe/java/group8/lab7/Yury/varA4/Main.java
+++ src/bsu/rfe/java/group8/lab7/Yury/varA4/Main.java
@@ -41,7 +41,89 @@ public class MainFrame extends JFrame {
     private final JTextArea textAreaIncoming;
     private final JTextArea textAreaOutgoing;
 
-    public static void main(String[] args) {
+    public MainFrame() {
+        super(FRAME_TITLE);
+        setMinimumSize(
+                new Dimension(FRAME_MINIMUM_WIDTH, FRAME_MINIMUM_HEIGHT));
+        // Центрирование окна
+        final Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - getWidth()) / 2,
+                (kit.getScreenSize().height - getHeight()) / 2);
+// Текстовая область для отображения полученных сообщений
+        textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
+        textAreaIncoming.setEditable(false);
+// Контейнер, обеспечивающий прокрутку текстовой области
+        final JScrollPane scrollPaneIncoming =
+                new JScrollPane(textAreaIncoming);
+// Подписи полей
+        final JLabel labelFrom = new JLabel("Подпись");
+        final JLabel labelTo = new JLabel("Получатель");
+// Поля ввода имени пользователя и адреса получателя
+        textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
+        textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
+// Текстовая область для ввода сообщения
+        textAreaOutgoing = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
+// Контейнер, обеспечивающий прокрутку текстовой области
+        final JScrollPane scrollPaneOutgoing =
+                new JScrollPane(textAreaOutgoing);
+// Панель ввода сообщения
+        final JPanel messagePanel = new JPanel();
+        messagePanel.setBorder(
+                BorderFactory.createTitledBorder("Сообщение"));
+// Кнопка отправки сообщения
+        final JButton sendButton = new JButton("Отправить");
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+// Компоновка элементов панели "Сообщение"
+        final GroupLayout layout2 = new GroupLayout(messagePanel);
+        messagePanel.setLayout(layout2);
+        layout2.setHorizontalGroup(layout2.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout2.createParallelGroup(Alignment.TRAILING)
+                        .addGroup(layout2.createSequentialGroup()
+                                .addComponent(labelFrom)
+                                .addGap(SMALL_GAP)
+                                .addComponent(textFieldFrom)
+                                .addGap(LARGE_GAP)
+                                .addComponent(labelTo)
+                                .addGap(SMALL_GAP)
+                                .addComponent(textFieldTo))
+                        .addComponent(scrollPaneOutgoing)
+                        .addComponent(sendButton))
+                .addContainerGap());
+        layout2.setVerticalGroup(layout2.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout2.createParallelGroup(Alignment.BASELINE)
+                        .addComponent(labelFrom)
+                        .addComponent(textFieldFrom)
+                        .addComponent(labelTo)
+                        .addComponent(textFieldTo))
+                .addGap(MEDIUM_GAP)
+                .addComponent(scrollPaneOutgoing)
+                .addGap(MEDIUM_GAP)
+                .addComponent(sendButton)
+                .addContainerGap());
+// Компоновка элементов фрейма
+        final GroupLayout layout1 = new GroupLayout(getContentPane());
+        setLayout(layout1);
+        layout1.setHorizontalGroup(layout1.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout1.createParallelGroup()
+                        .addComponent(scrollPaneIncoming)
+                        .addComponent(messagePanel))
+                .addContainerGap());
+        layout1.setVerticalGroup(layout1.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(scrollPaneIncoming)
+                .addGap(MEDIUM_GAP)
+                .addComponent(messagePanel)
+                .addContainerGap());
+
+        public static void MainFrame(String[] args) {
 	// write your code here
     }
 }
